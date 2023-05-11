20067c6d2b51b1b44f82a0f071c31d6bead46e06
bibil77 <benjamin.dupin@efrei.net>
2023-05-11 17:08:02 +0200

    change color for traduc link and merge component layout in header

--- component/Header.js
+++ component/Header.js
@@ -1,134 +1,226 @@
-import {
-  Avatar,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  Stack,
-  Typography,
-  Link,
-} from "@mui/material";
-import { useTranslation } from "next-i18next";
 import { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import { Link, Stack } from "@mui/material";
+import { useTranslation } from "next-i18next";
 
-import Layout from "./Layout";
-export default function Header() {
+const pages = ["À Propos de moi", "Compétence", "Centre D'intérêt"];
+
+function Header() {
   const { t } = useTranslation("header");
-  const [lang, setLang] = useState("fr");
+  const settings = [t("lang.fr"), t("lang.en")];
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
 
-  const handleChange = (event) => {
-    setLang(event.target.value);
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
   };
 
-  return <Layout />;
-}
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-{
-  /* <Stack>
-      <Layout />
-      <Stack
-        direction="row"
-        justifyContent="left"
-        sx={{
-          backgroundColor: "#868a8d",
-          color: "#fff",
-          pb: 2,
-          pt: 1,
-        }}
-      >
-        {/* photo de profil */
-}
-// <Avatar
-//   alt="Benjamin DUPIN"
-//   src="/images/moi1.jpg"
-//   sx={{
-//     display: { xs: "none", sm: "flex" },
-//     width: 200,
-//     height: 200,
-//     borderRadius: 5,
-//     border: "4px solid #4f5050",
-//     ml: 2,
-//   }}
-// >
-//   BD
-// </Avatar>
-{
-  /* photo de profil for xs*/
-}
-// <Avatar
-//   alt="Benjamin DUPIN"
-//   src="/images/moi1.jpg"
-//   sx={{
-//     display: { xs: "flex", sm: "none" },
-//   }}
-// >
-//   BD
-// </Avatar> */}
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-{
-  /* <Stack ml={7}>
-          <Typography
-            variant="h4"
-            component="h1"
+  return (
+    <AppBar position="static" sx={{ backgroundColor: "#868a8d" }}>
+      <Container maxWidth="xl" sx={{ py: 1 }}>
+        <Toolbar disableGutters sx={{ alignItems: "start" }}>
+          <Avatar
+            alt="Benjamin Dupin"
+            src="./images/moi1.jpg"
             sx={{
-              fontWeight: 500,
+              display: { xs: "none", sm: "flex" },
+              width: 200,
+              height: 200,
+              borderRadius: 5,
+              border: "4px solid #4f5050",
+            }}
+          />
+          <Stack>
+            <Typography
+              noWrap
+              variant="h4"
+              component="h1"
+              href="/"
+              sx={{
+                ml: 3,
+                mr: 2,
+                mt: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              BENJAMIN DUPIN
+            </Typography>
+            <Typography
+              variant="h6"
+              component="h2"
+              noWrap
+              href="/"
+              sx={{
+                ml: 6,
+                mt: 1,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              {t("title")}
+            </Typography>
+          </Stack>
+
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "flex", md: "none" },
             }}
           >
-            BENJAMIN DUPIN
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
           </Typography>
-          <Typography variant="h5" component="h2" mt={2} ml={2}>
-            {t("title")}
-          </Typography>
-        </Stack> */
-}
-{
-  /* </Stack> */
-}
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              justifyContent: "end",
+              mr: 2,
+            }}
+          >
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
 
-{
-  /* <FormControl
-        sx={{
-          ml: "auto",
-          mr: 2,
-          color: "#000",
-          "&:focus": {
-            borderColor: "#000",
-          },
-        }}
-      >
-        <InputLabel id="demo-simple-select-label">{t("lang.title")}</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={lang}
-          label={t("lang.title")}
-          onChange={handleChange}
-        >
-          <MenuItem value={"fr"} sx={{ padding: 0 }}>
-            <Link
-              href="/fr"
-              underline="none"
-              color="#000"
-              textAlign="center"
-              sx={{ width: "100%", py: 1 }}
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar
+                  alt="Benjamin DUPIN"
+                  src={t("lang.flag")}
+                  sx={{ mt: 1.5 }}
+                >
+                  BD
+                </Avatar>
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
             >
-              {t("lang.fr")}
-            </Link>
-          </MenuItem>
-          <MenuItem value={"en"} sx={{ padding: 0 }}>
-            <Link
-              href="/en"
-              underline="none"
-              color="#000"
-              textAlign="center"
-              sx={{ width: "100%", py: 1 }}
-            >
-              {t("lang.en")}
-            </Link>
-          </MenuItem>
-        </Select>
-      </FormControl> */
+              {settings.map((setting, i) => (
+                <MenuItem
+                  key={setting}
+                  onClick={handleCloseUserMenu}
+                  sx={{ padding: 0 }}
+                >
+                  <Link
+                    href={i === 0 ? "/fr" : "/en"}
+                    sx={{ width: "100%", py: 1, px: 1.5 }}
+                    underline="none"
+                    color="#000"
+                  >
+                    {setting}
+                  </Link>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
-{
-  /* </Stack> */
-}
+export default Header;
