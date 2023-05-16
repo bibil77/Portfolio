b89ba6c4f4985977481a64de538d08f06d117309
bibil77 <94520627+bibil77@users.noreply.github.com>
2023-05-16 11:25:06 +0200

    Color (#2)
    [portfolio] change theme colors
    * test
    
    * change color theme
    
    ---------
    
    Co-authored-by: bibil77 <benjamin.dupin@efrei.net>

--- src/theme.js
+++ src/theme.js
@@ -13,10 +13,10 @@ export const roboto = Roboto({
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#556cd6",
+      main: "#868a8d",
     },
     secondary: {
-      main: "#19857b",
+      main: "#9ea1a4",
     },
     error: {
       main: red.A400,
